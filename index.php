1def132b070811bcba7551d625191e0e006a5773
ashkanemami05 <ashkanemami2005@gmail.com>
2023-07-13 10:35:34 +0330

    transfer project

---
+++ index.php
@@ -0,0 +1,59 @@
+<html lang="fa">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <title>Home</title>
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.css">
+    <link rel="stylesheet" href="style/sryle.css">
+
+</head>
+<body>
+
+
+<nav class="navbar navbar-expand-lg bg-body-tertiary container" dir="rtl">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">سایت نمونه</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">خانه</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">بلاگ</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">فروشگاه</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">درباره ما</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">تماس با ما</a>
+                </li>
+
+
+
+            </ul>
+            <button class="btn btn-outline-success" type="submit">ورود</button>
+            &nbsp;
+            <button class="btn btn-success" type="submit">عضویت</button>
+
+        </div>
+    </div>
+</nav>
+
+
+
+
+</body>
+<script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="js/file.js"></script>
+<script src="node_modules/bootstrap/dist/js/bootstrap.js"></script>
+</html>
